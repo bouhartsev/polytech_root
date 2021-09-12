19187b9486b423c20bea93a2f0317d14f329f573
Matvey Bouhartsev <programmer@bouhartsev.top>
2021-09-12 22:39:29 +0300

    Start from

---
+++ index.php
@@ -0,0 +1,31 @@
+﻿<!doctype html>
+<html lang="ru">
+	
+<head>
+	<meta charset="utf-8">
+	<title>WEBLAB</title>
+	<link rel="stylesheet" href="./style.css">
+	<link rel="shortcut icon" href="./favicon.ico">
+</head>
+<body>
+	<h1>WEBLAB</h1>
+	<h2>by Matvey Bouharstev</h2>
+	<ul>
+		<?php 
+		$courses = scandir('./');
+		sort($courses, SORT_NATURAL | SORT_FLAG_CASE);
+		for ($i=2; $i<count($courses); $i++) {
+			if (is_dir($courses[$i])) {
+				echo '<li>'.$courses[$i].'<ul>';
+				$labs = scandir('./'.$courses[$i]);
+				sort($labs, SORT_NATURAL | SORT_FLAG_CASE);
+				for ($j=2; $j<count($labs); $j++) {
+					if (is_dir('./'.$courses[$i].'/'.$labs[$j])) echo '<li><a href="./'.$courses[$i].'/'.$labs[$j].'/">'.$labs[$j].'</a></li>';
+				}
+				echo '</ul></li>';
+			}
+		}
+		?>
+	</ul>
+</body>
+</html>
